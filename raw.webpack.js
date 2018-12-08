ac25d63ac853cd5e5409510e8a2b92fad20297d7
ngldev <angel@messingbit.com>
2018-12-08 19:52:59 +0100

    Add vuex and svgjs. Remove unnecessary example component

---
+++ raw.webpack.js
@@ -0,0 +1,9 @@
+var path = require('path')
+
+module.exports = {
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources'),
+    },
+  },
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const webpackConfig = require('./raw.webpack')
 
 /*
  |--------------------------------------------------------------------------
@@ -13,3 +14,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.webpackConfig({...webpackConfig})
